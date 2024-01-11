dae3957cb570036fa5da72ca5a7121bf75d9fc86
puturangga21 <puturangga21@gmail.com>
2024-01-11 22:53:00 +0800

    refactor: separating and create new component

---
+++ src/components/CardNews.jsx
@@ -0,0 +1,27 @@
+export default function CardNews({ date, title, description, img }) {
+  return (
+    <div className="flex flex-col items-center justify-between rounded-[20px] bg-white md:flex-row">
+      <div className="px-4 pt-5 md:w-[390px] md:px-0 md:py-[26px] md:pl-[26px] lg:w-[610px] lg:p-7 ">
+        <p className="mb-[20px] font-display text-xs font-medium uppercase text-gray-500 lg:mb-[30px]">
+          {date}
+        </p>
+
+        <h1 className="mb-4 font-display text-lg font-semibold leading-[25px] text-slate-800 md:mb-[10px] md:text-[22px] lg:mb-[10px] lg:text-[26px] lg:leading-[33px]">
+          {title}
+        </h1>
+
+        <p className="hidden text-lg leading-[27px] text-slate-600 md:block md:text-base">
+          {description}
+        </p>
+      </div>
+
+      <div className="h-[180px] overflow-hidden rounded-b-[10px] md:m-4 md:h-[170px] md:w-[210px] md:rounded-[10px] lg:m-7 lg:h-[210px] lg:w-[300px]">
+        <img
+          src={img}
+          alt="Thumbnail"
+          className="w-full object-cover md:h-full"
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/FirstNews.jsx
+++ src/components/FirstNews.jsx
@@ -1,3 +1,4 @@
+import CardNews from "./CardNews";
 import HeadingNews from "./HeadingNews";
 import Layout from "./Layout";
 
@@ -8,31 +9,12 @@ export default function FirstNews() {
         <HeadingNews title="CNBC" textColor="text-slate-800" />
 
         <div className="mt-7 flex w-[343px] flex-col gap-4 md:w-[670px] lg:mt-8 lg:w-full lg:gap-5">
-          <div className="flex flex-col items-center justify-between rounded-[20px] bg-white md:flex-row">
-            <div className="px-4 pt-5 md:w-[390px] md:px-0 md:py-[26px] md:pl-[26px] lg:w-[610px] lg:p-7 ">
-              <p className="mb-[20px] font-display text-xs font-medium uppercase text-gray-500 lg:mb-[30px]">
-                August 13, 2021
-              </p>
-
-              <h1 className="mb-4 font-display text-lg font-semibold leading-[25px] text-slate-800 md:mb-[10px] md:text-[22px] lg:mb-[10px] lg:text-[26px] lg:leading-[33px]">
-                10 Hilarious Cartoons That Depict Real-Life Problems of
-                Programmers
-              </h1>
-
-              <p className="hidden text-lg leading-[27px] text-slate-600 md:block md:text-base">
-                Redefined the user acquisition and redesigned the onboarding
-                experience, all within 3 working weeks.
-              </p>
-            </div>
-
-            <div className="h-[180px] overflow-hidden rounded-b-[10px] md:m-4 md:h-[170px] md:w-[210px] md:rounded-[10px] lg:m-7 lg:h-[210px] lg:w-[300px]">
-              <img
-                src="./thumbnail.jpg"
-                alt="Thumbnail"
-                className="w-full object-cover md:h-full"
-              />
-            </div>
-          </div>
+          <CardNews
+            date="August 13, 2021"
+            title="10 Hilarious Cartoons That Depict Real-Life Problems of Programmers"
+            description="Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks."
+            img="./thumbnail.jpg"
+          />
         </div>
       </div>
     </Layout>
