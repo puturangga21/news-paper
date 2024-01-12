851be35fd0655d54c7670442c1e6b84fae2b9165
puturangga21 <puturangga21@gmail.com>
2024-01-12 23:26:17 +0800

    feat: Add @vercel/analytics package and import Analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import SecondNews from "./components/SecondNews";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <FirstNews />
       <SecondNews />
       <Footer />
+      <Analytics />
     </>
   );
 }
